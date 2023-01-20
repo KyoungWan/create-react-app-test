2f90a1343ca4e6b6923586321b3f2d41c0bec814
kyoungwan.chae <kyngwan@gmail.com>
2023-01-20 18:16:00 +0900

    api

---
+++ api/index.js
@@ -0,0 +1,8 @@
+// /api/index.js
+import fetch from "node-fetch";
+
+export default async function handler(request, response) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  return response.status(200).json({ data });
+}
